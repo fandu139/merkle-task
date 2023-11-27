0b9ec69ec507475efc8a7d8e4b5486d7b3f338ba
fandu139 <fandupratama@gmail.com>
2023-11-28 00:10:22 +0800

    handle list user, integrated API and navigate user

--- src/views/User/Detail/User.js
+++ src/views/User/Detail/User.js
@@ -1,31 +1,81 @@
-import useForm from '../../../hook/useFormLogin';
-import validate from '../../../utils/validateForm';
-import { useState } from 'react';
+import { memo, useEffect, useMemo, useState } from 'react';
 import Pagination from 'react-bootstrap/Pagination';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import { useNavigate } from "react-router-dom";
 
 function DetailUser() {
-  let active = 1;
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [activePage, setActivePage] = useState(1);
+
   let items = [];
   for (let number = 1; number <= 5; number++) {
     items.push(
-      <Pagination.Item key={number} active={number === active}>
+      <Pagination.Item key={number} active={number === activePage} onClick={() => handleMovePagination(number)}>
         {number}
       </Pagination.Item>,
     );
   }
 
+  const handleMovePagination = (number) => {
+    setActivePage(number)
+    setLimit(number * 10)
+  }
+
+  useEffect(() => {
+    const getFirstDataUser = async () => {
+      await fetch(`https://fakestoreapi.com/users?limit=${limit}`)
+        .then(res=>res.json())
+        .then(json => {
+          const dataUser = user.length ? json.slice(limit - 10, limit) : json.slice(0, 10);
+          setUser(dataUser)
+        });
+    } 
+
+    getFirstDataUser();
+  }, [limit]);
+
+  const ShowDataUser = memo(({ id, email, username }) => {
+    return (
+      <tr>
+        <td>{id}</td>
+        <td>{email}</td>
+        <td>{username}</td>
+      </tr>
+    )
+  })
+
+  const handleNavigateAddUser = () => {
+    navigate('/user/add')
+  }
+
   return (
     <Container>
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {user.length ? user?.map((item, index) => <ShowDataUser key={index} id={item?.id} email={item?.email} username={item?.username} />) : null}
+        </tbody>
+      </Table>
       <Row>
-        <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
-        <Col>2 of 2</Col>
-      </Row>
-      <Row>
-        <Pagination>{items}</Pagination>
+        <Col sm={9}>
+          <Pagination>{items}</Pagination>
+        </Col>
+        <Col>
+          <Button variant="primary" onClick={handleNavigateAddUser}>Tambah User</Button>
+        </Col>
       </Row>
     </Container>
   );
